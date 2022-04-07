cbd031a99c65d44e790944aabfa448f04624e664
Edward-Leopold <verybusyman17@gmail.com>
2022-04-07 21:29:14 +0300

    Обработчик формы для отправки на почту
    Написал обработчик формы для отправки данных с формы на свою почту. Получилось коряво. На локальном сервере не работает.

--- form.php
+++ form.php
@@ -1,5 +1,26 @@
-<?php 
+<?php
+	// функция очистки приходящей строки
+	function clearStr($data){
+		return urldecode(htmlspecialchars(trim(strip_tags($data)))); 
+	}
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		// прием данных 
+		$name = clearStr($_POST["name"]);
+		$email = clearStr($_POST["email"]);
+		$msg = clearStr($_POST["msg"]);
+
+		// подготовка данных для отпарвки по почте
+		$to  = "=?utf-8?b?sodaniil4489@gmail.com?=";
+		$subject = "=?utf-8?b?Сообщение с сайта?=";
+		// str_replace("\n.", "\n..", $text)
+		$sender = "=?utf-8?B?" . base64_encode($name) . "?= <" . $email . ">";
+		$msg = "=?utf-8?B?" . base64_encode($msg) . "?=";
+
+		// функция отправки сообщения на электронную почту
+		mail($to, $subject, $msg, 'From: ' . $sender);
 		
+		// переадресация обратно на страницу формы
+		header("Location: index.php#contacts");
+		exit;
 	}
  ?>
